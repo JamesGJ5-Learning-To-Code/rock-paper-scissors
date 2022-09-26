6e0a016dcc2a88bb36f1eb5944986f0804dfa695
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-26 20:54:17 +0100

    Make showRoundResult() helper function for playRound()

--- javascript.js
+++ javascript.js
@@ -22,17 +22,20 @@ function getRoundResult(playerSelection, computerSelection) {
     return result;
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    let result
-    result = getRoundResult(playerSelection, computerSelection)
-    // TODO: make the below into a helper function
+function showRoundResult(result, playerSelection, computerSelection) {
     let action = "";
     if (result !== "Draw") {
         action = (result === "Win") ?
         ` ${playerSelection} beats ${computerSelection}!` : ` ${playerSelection} is beaten by ${computerSelection}!`;
     }
     console.log(`You ${result} this Round!${action}`);
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let result
+    result = getRoundResult(playerSelection, computerSelection)
+    showRoundResult(result, playerSelection, computerSelection);
     return result;
 }
 
