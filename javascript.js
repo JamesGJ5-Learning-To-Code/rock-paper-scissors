ffdba5c3a578cee78f996fbd80255737ddb3c787
James Graca-Jones <jamesgracajones@gmail.com>
2022-09-26 19:49:14 +0100

    Clean up
    Get rid of TODO comments
    
    Add semi-colons where appropriate
    
    Switch intermediate choice variables with return statements in
    getComputerChoice
    
    Remove console.log() statements
    
    Remove code outside of function bodies

--- javascript.js
+++ javascript.js
@@ -1,39 +1,24 @@
 function getComputerChoice () {
-    // TODO: get rid of console.log() statements and return choices rather than 
-    // assigning them to variables
-    choiceInteger = Math.floor(Math.random() * 3)
-    // console.log(choiceInteger)
+    let choiceInteger = Math.floor(Math.random() * 3);
     if (choiceInteger === 0) {
-        choice = "Rock"
+        return "Rock";
     } else if (choiceInteger === 1) {
-        choice = "Paper"
+        return "Paper";
     } else {
-        choice = "Scissors"
+        return "Scissors";
     }
-    // console.log(choice)
-    return choice
 }
 
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection)
-    console.log(computerSelection)
-
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
-    // console.log(playerSelection)
-
-    // Rock beats Scissors
-    // Paper beats Rock
-    // Scissors beats Paper
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let result
     if (playerSelection === computerSelection) {
-        result = "Draw"
+        result = "Draw";
     } else {
         result = (playerSelection === "Rock" && computerSelection === "Scissors" || 
                   playerSelection === "Paper" && computerSelection === "Rock" || 
                   playerSelection === "Scissors" && computerSelection === "Paper")
-                  ? "Win" : "Lose"
+                  ? "Win" : "Lose";
     }
-    // console.log(result)
-    return result
+    return result;
 }
-
-playRound("rock", getComputerChoice())
